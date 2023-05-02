4623d14cb11eab9c410e3a310994bb836abcdaf2
juuaronzon <131707113+juuaronzon@users.noreply.github.com>
2023-05-02 09:35:48 -0300

    Add files via upload

---
+++ Lineas Prog Redes/Lineas Prog Redes/Form1.Designer.vb
@@ -0,0 +1,95 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()>
+Partial Class Form1
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()>
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()>
+    Private Sub InitializeComponent()
+        Me.ProgressBar1 = New System.Windows.Forms.ProgressBar()
+        Me.txtNumLineas = New System.Windows.Forms.TextBox()
+        Me.btnIniciar = New System.Windows.Forms.Button()
+        Me.btnCancelar = New System.Windows.Forms.Button()
+        Me.BackgroundWorker1 = New System.ComponentModel.BackgroundWorker()
+        Me.SuspendLayout()
+        '
+        'ProgressBar1
+        '
+        Me.ProgressBar1.ForeColor = System.Drawing.Color.FromArgb(CType(CType(255, Byte), Integer), CType(CType(128, Byte), Integer), CType(CType(255, Byte), Integer))
+        Me.ProgressBar1.Location = New System.Drawing.Point(126, 331)
+        Me.ProgressBar1.Name = "ProgressBar1"
+        Me.ProgressBar1.Size = New System.Drawing.Size(493, 39)
+        Me.ProgressBar1.TabIndex = 0
+        '
+        'txtNumLineas
+        '
+        Me.txtNumLineas.Font = New System.Drawing.Font("Century Gothic", 18.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point)
+        Me.txtNumLineas.Location = New System.Drawing.Point(126, 124)
+        Me.txtNumLineas.Name = "txtNumLineas"
+        Me.txtNumLineas.Size = New System.Drawing.Size(268, 37)
+        Me.txtNumLineas.TabIndex = 1
+        '
+        'btnIniciar
+        '
+        Me.btnIniciar.Font = New System.Drawing.Font("Century Gothic", 27.75!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point)
+        Me.btnIniciar.Location = New System.Drawing.Point(126, 190)
+        Me.btnIniciar.Name = "btnIniciar"
+        Me.btnIniciar.Size = New System.Drawing.Size(218, 96)
+        Me.btnIniciar.TabIndex = 2
+        Me.btnIniciar.Text = "Iniciar escritura"
+        Me.btnIniciar.UseVisualStyleBackColor = True
+        '
+        'btnCancelar
+        '
+        Me.btnCancelar.Font = New System.Drawing.Font("Century Gothic", 27.75!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point)
+        Me.btnCancelar.Location = New System.Drawing.Point(371, 190)
+        Me.btnCancelar.Name = "btnCancelar"
+        Me.btnCancelar.Size = New System.Drawing.Size(218, 96)
+        Me.btnCancelar.TabIndex = 3
+        Me.btnCancelar.Text = "Parar escritura"
+        Me.btnCancelar.UseVisualStyleBackColor = True
+        '
+        'BackgroundWorker1
+        '
+        Me.BackgroundWorker1.WorkerReportsProgress = True
+        Me.BackgroundWorker1.WorkerSupportsCancellation = True
+        '
+        'Form1
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(7.0!, 15.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.ClientSize = New System.Drawing.Size(800, 450)
+        Me.Controls.Add(Me.btnCancelar)
+        Me.Controls.Add(Me.btnIniciar)
+        Me.Controls.Add(Me.txtNumLineas)
+        Me.Controls.Add(Me.ProgressBar1)
+        Me.Name = "Form1"
+        Me.Text = "Form1"
+        Me.ResumeLayout(False)
+        Me.PerformLayout()
+
+    End Sub
+
+    Friend WithEvents ProgressBar1 As ProgressBar
+    Friend WithEvents txtNumLineas As TextBox
+    Friend WithEvents btnIniciar As Button
+    Friend WithEvents btnCancelar As Button
+    Friend WithEvents BackgroundWorker1 As System.ComponentModel.BackgroundWorker
+
+End Class
---
+++ Lineas Prog Redes/Lineas Prog Redes/Form1.vb
@@ -0,0 +1,43 @@
+﻿Imports System.IO
+Public Class Form1
+
+    Private Sub btnIniciar_Click(sender As Object, e As EventArgs) Handles btnIniciar.Click
+        Dim numLineas As Integer = Integer.Parse(txtNumLineas.Text)
+        BackgroundWorker1.RunWorkerAsync(numLineas)
+    End Sub
+
+    Private Sub btnCancelar_Click(sender As Object, e As EventArgs) Handles btnCancelar.Click
+        BackgroundWorker1.CancelAsync()
+    End Sub
+
+    Private Sub BackgroundWorker1_DoWork(sender As Object, e As System.ComponentModel.DoWorkEventArgs) Handles BackgroundWorker1.DoWork
+        Dim numLineas As Integer = CType(e.Argument, Integer)
+
+        Using sw As StreamWriter = New StreamWriter("Archivo.txt")
+            For i As Integer = 1 To numLineas
+                sw.WriteLine("Linea " & i.ToString())
+                Dim porcentaje As Integer = CInt((CDbl(i) / CDbl(numLineas)) * 100)
+                BackgroundWorker1.ReportProgress(porcentaje)
+
+                If BackgroundWorker1.CancellationPending Then
+                    e.Cancel = True
+                    Exit For
+                End If
+            Next
+        End Using
+    End Sub
+
+    Private Sub BackgroundWorker1_ProgressChanged(sender As Object, e As System.ComponentModel.ProgressChangedEventArgs) Handles BackgroundWorker1.ProgressChanged
+        ' Actualizar la barra de progreso
+        ProgressBar1.Value = e.ProgressPercentage
+    End Sub
+
+    Private Sub BackgroundWorker1_RunWorkerCompleted(sender As Object, e As System.ComponentModel.RunWorkerCompletedEventArgs) Handles BackgroundWorker1.RunWorkerCompleted
+        If e.Cancelled Then
+            MessageBox.Show("Proceso cancelado por el usuario.")
+        Else
+            MessageBox.Show("Archivo creado correctamente.")
+        End If
+    End Sub
+
+End Class
